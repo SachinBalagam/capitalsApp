e17c810f6cd40acfe87f5277fdb1d79005a32370
SachinBalagam <balagamsachin337@gmail.com>
2023-08-02 16:53:55 +0000

    Final Commit

--- src/components/Capitals/index.js
+++ src/components/Capitals/index.js
@@ -1,3 +1,6 @@
+import {Component} from 'react'
+import './index.css'
+
 const countryAndCapitalsList = [
   {
     id: 'NEW_DELHI',
@@ -27,3 +30,47 @@ const countryAndCapitalsList = [
 ]
 
 // Write your code here
+class Capitals extends Component {
+  state = {
+    ActiveCountryId: countryAndCapitalsList[0].id,
+  }
+
+  onOptionChange = event => {
+    this.setState({
+      ActiveCountryId: event.target.value,
+    })
+  }
+
+  render() {
+    const {ActiveCountryId} = this.state
+    const Active = countryAndCapitalsList.filter(
+      each => each.id === ActiveCountryId,
+    )
+    const {country} = Active[0]
+
+    return (
+      <div className="bg-container">
+        <div className="card">
+          <h1>Countries and Capitals</h1>
+          <div className="question-container">
+            <select
+              className="select-style"
+              value={ActiveCountryId}
+              onChange={this.onOptionChange}
+            >
+              {countryAndCapitalsList.map(each => (
+                <option value={each.id} key={each.id}>
+                  {each.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <p className="question">is capital of which country</p>
+          </div>
+          <h1>{country}</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Capitals
